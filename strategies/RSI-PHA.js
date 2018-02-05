21e3bf5aa29f41a8fb790a9e42d8759e5af12300
Anh Pha <phavo@minhhungland.vn>
2018-02-05 11:26:55 +0700

    working on custom RSI

--- strategies/RSI-PHA.js
+++ strategies/RSI-PHA.js
@@ -10,7 +10,7 @@ var _ = require('lodash');
 var log = require('../core/log.js');
 
 var RSI = require('./indicators/RSI.js');
-var SMA = require('./indicators/SMA.js');
+var SMA = require('./indicators/MACD.js');
 
 // let's create our own method
 var method = {};
@@ -18,6 +18,20 @@ var method = {};
 // prepare everything our method needs
 method.init = function() {
   this.name = 'RSI-PHA';
+  this.downCount = 0;
+
+  this.lastCandle;
+  this.currentCandle;
+  this.tradeCount = 0;
+
+  this.lastEMA = 0;
+  this.currentEMA = 0;
+  this.maxMACDShortAfterUp = 0;
+  this.macdDownCrossedAfterBuy = false;
+  this.maxRSIAfterMACDUp = 0;
+
+  this.macdVals = [];
+  this.rsis = [];
 
   this.trend = {
     direction: 'none',
@@ -34,16 +48,32 @@ method.init = function() {
     duration: 0,
     persisted: false,
     adviced: false,
+    sellOnHigh: false,
   };
   this.dmaTrend = '';
 
   this.requiredHistory = this.tradingAdvisor.historySize;
 
   // define the indicators we need
-  this.addIndicator('rsi', 'RSI', this.settings);
+  this.addIndicator('rsi', 'RSI', { interval: 14 });
+  log.debug('This settings: ', this.settings);
   // this.addIndicator('sma', 'SMA', 9);
   // this.addIndicator('dema', 'DEMA', this.settings.dma);
   this.addIndicator('macd', 'MACD', this.settings.macd);
+};
+
+method.getCandleHeight = function(candle) {
+  return candle.close - candle.open;
+};
+
+method.IsAtTooHigh = function(lastCandle, currentCandle) {
+  if (
+    this.getCandleHeight(currentCandle) > 0 &&
+    this.getCandleHeight(currentCandle) > 0.05 * currentCandle.close
+  ) {
+    return true;
+  }
+  return false;
 };
 
 // for debugging purposes log the last
@@ -70,13 +100,19 @@ method.log = function(candle) {
   }
 };
 
+method.runMACD = function(candle) {
+  return this.buyOnMACDCross(candle);
+};
+
 method.buyOnMACDCross = function(candle) {
   var macddiff = this.indicators.macd.result;
   var rsiVal = this.indicators.rsi.result;
 
+  // log.debug('Running MACD: ', macddiff, ' - Time: ', candle.start);
+
   if (macddiff > this.settings.macd.up) {
     // new trend detected
-    if (this.macdTrend.direction !== 'up')
+    if (this.macdTrend.direction !== 'up') {
       // reset the state for the new trend
       this.macdTrend = {
         duration: 0,
@@ -84,28 +120,67 @@ method.buyOnMACDCross = function(candle) {
         direction: 'up',
         adviced: false,
       };
+      this.macdTrend.sellOnHigh = false;
+      this.maxMACDShortAfterUp = this.indicators.macd.diff;
+      this.macdVals = [];
+      this.macdVals.push(this.maxMACDShortAfterUp);
+      this.maxRSIAfterMACDUp = this.indicators.rsi.result;
+      this.rsis = [];
+      this.rsis.push(this.maxRSIAfterMACDUp);
+    }
 
     this.macdTrend.duration++;
 
-    if (this.macdTrend.duration >= this.settings.macd.persistence)
+    if (this.macdTrend.duration >= this.settings.macd.persistence) {
       this.macdTrend.persisted = true;
+    }
 
+    // log.debug(
+    //   '**** MACD on High',
+    //   this.macdTrend.duration,
+    //   'candle(s):',
+    //   candle.close,
+    //   '- Time: ',
+    //   candle.start,
+    //   '- Open <= CLose: ',
+    //   candle.open <= candle.close,
+    //   '- Persited: ',
+    //   this.macdTrend.persisted,
+    //   '- Adviced: ',
+    //   this.macdTrend.adviced,
+    //   '- Increase EMA: ',
+    //   this.maxMACDShortAfterUp - this.indicators.macd.diff
+    // );
+    // log.debug('- RSI Vals: ', this.rsis);
     if (
+      this.trend.buy == 0 &&
       this.macdTrend.persisted &&
       !this.macdTrend.adviced &&
-      rsiVal < this.settings.macd.rsi
+      rsiVal < this.settings.macd.rsi &&
+      this.macdTrend.duration < 8 &&
+      candle.close >= candle.open &&
+      !this.IsAtTooHigh(this.lastCandle, this.currentCandle) &&
+      this.maxMACDShortAfterUp == this.indicators.macd.diff &&
+      this.maxRSIAfterMACDUp == this.indicators.rsi.result &&
+      !this.macdTrend.sellOnHigh
     ) {
       log.debug(
         '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Buy on MACD cross',
         this.macdTrend.duration,
-        'candle(s)'
+        'candle(s):',
+        candle.close,
+        '- Time: ',
+        candle.start
       );
-      this.doBuy(candle);
+      // log.debug('- MACD Vals: ', this.macdVals);
+      // log.debug('- RSI Vals: ', this.rsis);
+      this.doBuy(candle.close);
       return true;
     }
   } else if (macddiff < this.settings.macd.down) {
     // new trend detected
-    if (this.macdTrend.direction !== 'down')
+    // Update trend but do nothing
+    if (this.macdTrend.direction !== 'down') {
       // reset the state for the new trend
       this.macdTrend = {
         duration: 0,
@@ -113,8 +188,42 @@ method.buyOnMACDCross = function(candle) {
         direction: 'down',
         adviced: false,
       };
+      // log.debug(
+      //   '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<On MACD down',
+      //   this.macdTrend.duration,
+      //   'candle(s):',
+      //   candle.close,
+      //   '- Time: ',
+      //   candle.start
+      // );
+
+      if (this.trend.buy > 0) {
+        this.macdDownCrossedAfterBuy = true;
+      }
+    }
 
     this.macdTrend.duration++;
+    if (this.macdTrend.duration >= 1) this.macdTrend.persisted = true;
+
+    if (
+      this.trend.buy > 0 &&
+      this.macdTrend.persisted &&
+      !this.macdTrend.adviced &&
+      this.macdDownCrossedAfterBuy
+    ) {
+      log.debug(
+        '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Sell on MACD cross',
+        this.macdTrend.duration,
+        'candle(s):',
+        candle.close,
+        '- Time: ',
+        candle.start
+      );
+      // this.macdDownCrossedAfterBuy = false;
+
+      this.doSell(candle.close);
+      return true;
+    }
   }
   return false;
 };
@@ -147,7 +256,33 @@ method.buyOnSAMCross = function(candle) {
   return false;
 };
 
-method.updateTrend = function(direction) {
+method.update = function(candle) {
+  this.lastCandle = this.currentCandle;
+  this.currentCandle = candle;
+  this.lastEMA = this.currentEMA;
+  this.currentEMA = this.indicators.macd.diff;
+  this.updateMaxMACDAfterUp(candle);
+  this.updateMaxRSI(candle);
+  this.downCount = candle.close < candle.open ? this.downCount + 1 : 0;
+};
+
+method.updateMaxMACDAfterUp = function(candle) {
+  this.maxMACDShortAfterUp =
+    this.currentEMA > this.maxMACDShortAfterUp
+      ? this.currentEMA
+      : this.maxMACDShortAfterUp;
+  this.macdVals.push(this.currentEMA);
+};
+
+method.updateMaxRSI = function(candle) {
+  this.maxRSIAfterMACDUp =
+    this.indicators.rsi.result > this.maxRSIAfterMACDUp
+      ? this.indicators.rsi.result
+      : this.maxRSIAfterMACDUp;
+  this.rsis.push(this.indicators.rsi.result);
+};
+
+method.updateRSITrend = function(direction) {
   // new trend detected
   if (this.trend.direction !== direction) {
     log.debug('Reset trend', this.trend.direction, direction);
@@ -163,10 +298,37 @@ method.updateTrend = function(direction) {
   this.trend.duration++;
 };
 
-method.sellOnHighRSI = function(rsiVal, candle) {
+/**
+ * Return true if there is any buy or sell action
+ * @param {*} rsiVal
+ * @param {*} candle
+ */
+method.runRSI = function(rsiVal, candle) {
+  if (rsiVal <= 0) {
+    log.debug('Not enought data for rsi');
+    return false;
+  }
+  if (this.checkSellOnHighRSI(rsiVal, candle)) {
+    return true;
+  } else {
+    return this.checkBuyOnLowRSI(rsiVal, candle);
+  }
+};
+
+method.checkSellOnHighRSI = function(rsiVal, candle) {
   if (rsiVal > this.settings.thresholds.high) {
     // new trend detected
-    this.updateTrend('high');
+    this.updateRSITrend('high');
+    // log.debug(
+    //   'In high since: - Duration: ',
+    //   this.trend.duration,
+    //   '- Buy: ',
+    //   this.trend.buy,
+    //   ' - Adviced: ',
+    //   this.trend.adviced,
+    //   '- Time: ',
+    //   candle.start
+    // );
     if (
       this.trend.duration >= this.settings.thresholds.highpersistence &&
       candle.low > this.trend.buy
@@ -178,7 +340,12 @@ method.sellOnHighRSI = function(rsiVal, candle) {
       log.debug(
         '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SELL:  In high since',
         this.trend.duration,
-        'candle(s)'
+        'candle(s):',
+        candle.close,
+        '- RSI: ',
+        rsiVal,
+        '- Time: ',
+        candle.start
       );
 
       this.doSell(candle.close);
@@ -190,27 +357,32 @@ method.sellOnHighRSI = function(rsiVal, candle) {
   return false;
 };
 
-method.buyOnLowRSI = function(rsiVal, candle) {
+method.checkBuyOnLowRSI = function(rsiVal, candle) {
   if (rsiVal < this.settings.thresholds.low) {
     // new trend detected
-    this.updateTrend('low');
+    this.updateRSITrend('low');
 
     if (this.trend.duration >= this.settings.thresholds.persistence) {
       this.trend.persisted = true;
     }
 
-    log.debug(
-      'Checking to buy:',
-      this.trend.buy,
-      this.trend.persisted,
-      this.trend.adviced,
-      this.trend.duration
-    );
+    // log.debug(
+    //   'Checking to buy:',
+    //   this.trend.buy,
+    //   this.trend.persisted,
+    //   this.trend.adviced,
+    //   this.trend.duration
+    // );
     if (this.trend.persisted && !this.trend.adviced && this.trend.buy == 0) {
       log.debug(
         '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<BUY: In low since',
         this.trend.duration,
-        'candle(s)'
+        'candle(s):',
+        candle.close,
+        '- RSI: ',
+        rsiVal,
+        '- Time: ',
+        candle.start
       );
       this.doBuy(candle.close);
     } else {
@@ -221,29 +393,69 @@ method.buyOnLowRSI = function(rsiVal, candle) {
   return false;
 };
 
-method.takeProfit = function(rsiVal, candle) {
+method.checkTakeProfit = function(rsiVal, candle) {
   if (
     rsiVal >= this.settings.minrsiprofit &&
     this.trend.buy > 0 &&
-    candle.high >= this.trend.buy * (1 + this.settings.microprofit / 100)
+    candle.close >= this.trend.buy * (1 + this.settings.microprofit / 100)
   ) {
-    log.debug('***********************************SELL: Got target trend');
+    log.debug(
+      '***********************************SELL: Got target trend: ',
+      100 * (candle.close - this.trend.buy) / this.trend.buy
+    );
     this.doSell(candle.close);
     return true;
   }
   return false;
 };
 
-method.stopLost = function(rsiVal, candle) {
+method.checkStopLost = function(rsiVal, candle) {
   if (rsiVal >= this.settings.checklost && this.trend.buy > candle.high) {
-    log.debug('***********************************SELL: Stop lost');
+    log.debug(
+      '***********************************SELL: Stop lost due to hight rsi but low price: ',
+      candle.close,
+      '- RSI: ',
+      rsiVal,
+      '- Time: ',
+      candle.start
+    );
     this.doSell(candle.close);
     return true;
   }
+  var maxLost = this.trend.buy * this.settings.maxloss / 100;
+  if (candle.low <= this.trend.buy - maxLost) {
+    log.debug(
+      '***********************************SELL: Stop loss due to low price: ',
+      candle.close,
+      '- Time: ',
+      candle.start,
+      '- Loss: ',
+      100 * (this.trend.buy - candle.low) / this.trend.buy,
+      '%'
+    );
+    this.doSell(candle.close);
+    return true;
+  }
+  // if (
+  //   this.trend.buy > 0 &&
+  //   rsiVal > this.settings.thresholds.low &&
+  //   this.downCount == 2
+  // ) {
+  //   log.debug(
+  //     '***********************************SELL: Stop loss due 2 down candles: ',
+  //     candle.close,
+  //     '- Time: ',
+  //     candle.start
+  //   );
+  //   this.doSell(candle.close);
+  //   return true;
+  // }
   return false;
 };
 
 method.doBuy = function(price) {
+  // log.debug('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<buy');
+  this.macdDownCrossedAfterBuy = false;
   this.trend.adviced = true;
   this.trend.action = true;
   this.advice('long');
@@ -252,11 +464,22 @@ method.doBuy = function(price) {
 };
 
 method.doSell = function(price) {
+  // log.debug('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Sell');
+  if (this.indicators.macd.result > this.settings.macd.up) {
+    this.macdTrend.sellOnHigh = true;
+  }
+  this.macdDownCrossedAfterBuy = false;
   this.trend.adviced = true;
   this.trend.action = true;
   this.advice('short');
   this.trend.sell = price;
   this.trend.buy = 0;
+  this.tradeCount++;
+  log.debug(
+    '>>>>>>>>>>>>>>>>>>>> Trade: ',
+    this.tradeCount,
+    '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
+  );
 };
 
 method.doNothing = function() {
@@ -265,138 +488,32 @@ method.doNothing = function() {
 };
 
 method.check = function(candle) {
-  var rsi = this.indicators.rsi;
-  var rsiVal = rsi.result;
+  var rsiVal = this.indicators.rsi.result;
+  var done = false;
 
-  if (rsiVal <= 0) {
-    log.debug('Not enought data for rsi');
-    this.doNothing();
-    return;
-  }
-
-  if (this.sellOnHighRSI(rsiVal, candle)) {
-    // log.debug('Done sell');
-    return;
-  }
-
-  // if (this.buyOnSAMCross(candle)) {
+  // if (rsiVal <= 0) {
+  //   log.debug('Not enought data for rsi');
+  //   this.doNothing();
   //   return;
   // }
-  if (this.buyOnLowRSI(rsiVal, candle)) {
-    // log.debug('Done slow', this.trend.duration);
-    return;
+  // Check RSI first
+  done = this.runRSI(rsiVal, candle);
+  // Check MACD
+  if (!done) {
+    done = this.runMACD(candle);
   }
-
-  if (this.takeProfit(rsiVal, candle)) {
-    // log.debug('Done take profit');
-    return;
+  // Check take profit when no action has been done
+  if (!done) {
+    done = this.checkTakeProfit(rsiVal, candle);
   }
-
-  if (this.stopLost(rsiVal, candle)) {
-    // log.debug('Done stoploss');
-    return;
+  // Check stoploss when no action has been done
+  if (!done) {
+    done = this.checkStopLost(rsiVal, candle);
   }
-  this.doNothing();
-
-  // if (rsiVal > this.settings.thresholds.high) {
-  //   // new trend detected
-  //   if (this.trend.direction !== 'high')
-  //     this.trend = {
-  //       duration: 0,
-  //       persisted: false,
-  //       direction: 'high',
-  //       adviced: false,
-  //       buy: this.trend.buy,
-  //       sell: this.trend.sell,
-  //     };
-
-  //   this.trend.duration++;
-
-  //   if (
-  //     this.trend.duration >= this.settings.thresholds.highpersistence &&
-  //     candle.low > this.trend.buy
-  //   ) {
-  //     this.trend.persisted = true;
-  //   }
-
-  //   if (this.trend.buy == 0) {
-  //     // log.debug('Not buy yet');
-  //     this.advice();
-  //     this.trend.action = false;
-  //     return;
-  //   }
-
-  //   if (this.trend.buy > 0 && this.trend.persisted && !this.trend.adviced) {
-  //     log.debug(
-  //       '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SELL:  In high since',
-  //       this.trend.duration,
-  //       'candle(s)'
-  //     );
-
-  //     log.debug('Detail:', rsiVal, candle);
-  //     this.trend.adviced = true;
-  //     this.trend.action = true;
-  //     this.advice('short');
-  //     this.trend.sell = candle.close;
-  //     this.trend.buy = 0;
-  //   } else {
-  //     this.advice();
-  //     this.trend.action = false;
-  //   }
-  // } else if (rsiVal < this.settings.thresholds.low) {
-  //   // new trend detected
-  //   if (this.trend.direction !== 'low')
-  //     this.trend = {
-  //       duration: 0,
-  //       persisted: false,
-  //       direction: 'low',
-  //       adviced: false,
-  //       buy: this.trend.buy,
-  //       sell: this.trend.sell,
-  //     };
-
-  //   this.trend.duration++;
-
-  //   if (this.trend.duration >= this.settings.thresholds.persistence) {
-  //     this.trend.persisted = true;
-  //   }
-
-  //   log.debug(this.trend.buy);
-  //   if (this.trend.persisted && !this.trend.adviced && this.trend.buy == 0) {
-  //     log.debug(
-  //       '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<BUY: In low since',
-  //       this.trend.duration,
-  //       'candle(s)'
-  //     );
-  //     log.debug('Detail:', rsiVal, candle);
-  //     this.trend.adviced = true;
-  //     this.advice('long');
-  //     this.trend.buy = candle.close;
-  //     this.trend.action = true;
-  //   } else {
-  //     this.trend.action = false;
-  //     this.advice();
-  //   }
-  // } else {
-  //   if (
-  //     rsiVal >= this.settings.minrsiprofit &&
-  //     this.trend.buy > 0 &&
-  //     candle.high >= this.trend.buy * (1 + this.settings.microprofit / 100)
-  //   ) {
-  //     log.debug('***********************************SELL: Got target trend');
-  //     log.debug('Detail:', rsiVal, candle);
-  //     this.trend.adviced = true;
-  //     this.trend.action = true;
-  //     this.advice('short');
-  //     this.trend.sell = candle.close;
-  //     this.trend.buy = 0;
-  //   } else {
-  //     // log.debug('In no trend');
-
-  //     this.advice();
-  //     this.trend.action = false;
-  //   }
-  // }
+  // If no action done, we advice do nothing
+  if (!done) {
+    this.doNothing();
+  }
 };
 
 module.exports = method;
--- strategies/RSI.js
+++ strategies/RSI.js
@@ -22,14 +22,14 @@ method.init = function() {
     direction: 'none',
     duration: 0,
     persisted: false,
-    adviced: false
+    adviced: false,
   };
 
   this.requiredHistory = this.tradingAdvisor.historySize;
 
   // define the indicators we need
   this.addIndicator('rsi', 'RSI', this.settings);
-}
+};
 
 // for debugging purposes log the last
 // calculated parameters.
@@ -40,66 +40,60 @@ method.log = function(candle) {
   log.debug('calculated RSI properties for candle:');
   log.debug('\t', 'rsi:', rsi.result.toFixed(digits));
   log.debug('\t', 'price:', candle.close.toFixed(digits));
-}
+  log.debug('- Time: ', candle.start);
+};
 
 method.check = function() {
   var rsi = this.indicators.rsi;
   var rsiVal = rsi.result;
 
-  if(rsiVal > this.settings.thresholds.high) {
-
+  if (rsiVal > this.settings.thresholds.high) {
     // new trend detected
-    if(this.trend.direction !== 'high')
+    if (this.trend.direction !== 'high')
       this.trend = {
         duration: 0,
         persisted: false,
         direction: 'high',
-        adviced: false
+        adviced: false,
       };
 
     this.trend.duration++;
 
     log.debug('In high since', this.trend.duration, 'candle(s)');
 
-    if(this.trend.duration >= this.settings.thresholds.persistence)
+    if (this.trend.duration >= this.settings.thresholds.persistence)
       this.trend.persisted = true;
 
-    if(this.trend.persisted && !this.trend.adviced) {
+    if (this.trend.persisted && !this.trend.adviced) {
       this.trend.adviced = true;
       this.advice('short');
-    } else
-      this.advice();
-
-  } else if(rsiVal < this.settings.thresholds.low) {
-
+    } else this.advice();
+  } else if (rsiVal < this.settings.thresholds.low) {
     // new trend detected
-    if(this.trend.direction !== 'low')
+    if (this.trend.direction !== 'low')
       this.trend = {
         duration: 0,
         persisted: false,
         direction: 'low',
-        adviced: false
+        adviced: false,
       };
 
     this.trend.duration++;
 
     log.debug('In low since', this.trend.duration, 'candle(s)');
 
-    if(this.trend.duration >= this.settings.thresholds.persistence)
+    if (this.trend.duration >= this.settings.thresholds.persistence)
       this.trend.persisted = true;
 
-    if(this.trend.persisted && !this.trend.adviced) {
+    if (this.trend.persisted && !this.trend.adviced) {
       this.trend.adviced = true;
       this.advice('long');
-    } else
-      this.advice();
-
+    } else this.advice();
   } else {
-
     log.debug('In no trend');
 
     this.advice();
   }
-}
+};
 
 module.exports = method;
